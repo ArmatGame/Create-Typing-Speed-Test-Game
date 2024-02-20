cd38006422c5d469c88c7e6ea56a79f524503f78
Ahmed Mohamed <106612109+ArmatGame@users.noreply.github.com>
2024-02-20 22:08:38 +0200

    Delete Create Typing Speed Test Game directory

--- Create Typing Speed Test Game/assets/dist/js/aa.js
+++
@@ -1,5 +0,0 @@
-document.body.addEventListener("paste", () => {
-  document.links[0].style.display = "block";
-  document.links[0].style.textDecoration = "none";
-  document.links[0].style.color = "red";
-})
